4eec49b5e2cc1e580671795cd35a920e463a9161
Denys Galante <denys.galante@br.experian.com>
2024-08-28 23:40:04 -0300

    update version for 6.15.1

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "GoogleMobileAdsMediationFacebook",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
